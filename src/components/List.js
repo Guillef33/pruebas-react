c2574606dc6754d9522d35f4bd707371022a50ac
Guillermo <guillef33@gmail.com>
2021-12-04 14:23:01 -0300

    ajustes borrado

--- src/components/List.js
+++ src/components/List.js
@@ -9,26 +9,26 @@ import props from "prop-types"; // Las props que va a recibir nuestro componente
 // Errores que encontramos cuando el codigo no genera lo que buscamos, son los mas dificiles de buscar
 
 function List() {
-
   const [regalo, setRegalo] = useState({
     addGift: "",
-    gifs: ["Medias", "VitelTone", "Caramelos"]
+    deleteGift: "",
+    gifs: ["Medias", "Vitel tone", "Caramelos"],
+    // gifs: [{id:"1", title:"Medias"}, {id:"2", title :"Vitel Tone"}, {id:"3", title : "Caramelos"}]
   });
 
   function handleChange(e) {
-    setRegalo({...regalo, addGift: e.target.value}); // mantiene lo que ya tiene y va agregando
+    setRegalo({ ...regalo, addGift: e.target.value }); // mantiene lo que ya tiene y va agregando
   }
 
   function handleSubmit(e) {
-    e.preventDefault()
-    // los regalos son inmutables, entonces tenemos que crear uno nuevo, en este caso newContainer: hacemos una copia y lo pusheamos
+    e.preventDefault();
     let newContainer = regalo.gifs;
     newContainer.push(regalo.addGift);
     setRegalo({
-      ...regalo, gifs: newContainer,
-      addGift: ""
-    })
-    // regalo.gifs.push(regalo);
+      ...regalo,
+      gifs: newContainer,
+      addGift: "",
+    });
   }
 
   return (
@@ -41,8 +41,7 @@ function List() {
               type="text"
               placeholder="Agrega regalo..."
               value={regalo.addGift}
-              onChange={handleChange}
-            />
+              onChange={handleChange}/>
             <input type="submit" value="Agregar" />
           </div>
         </form>
--- src/components/RegalosList.js
+++ src/components/RegalosList.js
@@ -1,21 +1,52 @@
-import React from "react";
+import React, { useState} from "react";
 
 import { faCandyCane } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function RegalosList ({lista}) { // Aca traemos la lista de regalos, destructuring porque sabemos el nombre para identificar a la prop que esta recibiendo el componente hijo
     
+//   const [regalo, setRegalo] = useState({
+//     addGift: "",
+//     deleteGift: "",
+//     gifs: ["Medias", "Vitel tone", "Caramelos"],
+//   });
+
+  const [borrarRegalo, setBorrarRegalo] = React.useState(lista);
+
+  const removeItem = (id) => {
+    let newPeople = borrarRegalo.filter((regalo) => regalo.id !== id);
+    setBorrarRegalo(newPeople);
+  };
+
+
+// //   function deleteArray(e) {
+//     let newContainer = regalo.gifs;
+//     newContainer.splice(regalo.deleteGift);
+//     setRegalo({
+//       ...regalo,
+//       gifs: newContainer,
+//       deleteGift: "",
+//     });
+
+    // const removeItem = {} => {
+    //     let newArray = 
+    // }
+
+
     return (
-        <>
+      <>
         {lista.map((item, i) => (
-            <li key={i}>
-                <FontAwesomeIcon icon={faCandyCane} />
-                {item}
-            </li>
-        ))
-        }
-        </>
-    )
-}
+          <li key={i}>
+            <FontAwesomeIcon icon={faCandyCane} />
+            {item}
+            <input onClick={() => removeItem(id)} value="borrar" />
+
+          </li>
+        ))}
+      </>
+    );
+};
+    
+    
 
 export default RegalosList;
